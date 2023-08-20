15dc870a162e779a82d7e9c0e357d1f38e28d69d
sophiaabolore <59152413+sophiaabolore@users.noreply.github.com>
2023-08-20 10:55:42 -0700

    Reconfigured the main script

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ def process_dataset():
     train_data.iloc[:, numerical_cols] = scaler.fit_transform(train_data.iloc[:, numerical_cols])
     print(train_data.iloc[:, numerical_cols].columns)
     test_data.iloc[:, numerical_cols] = scaler.fit_transform(test_data.iloc[:, numerical_cols])  # Use the same scaler on test data
-    print("After Scaling:", train_data.shape)
+
 
     # Convert column indices to column names for categorical columns
     categorical_colnames = train_data.columns[categorical_cols].tolist()
@@ -125,70 +125,6 @@ class IDSEnvironment(gym.Env):
     def close(self):
         pass
 
-
-class DQNAgent:
-    def __init__(self, state_size, action_size):
-        self.state_size = state_size
-        self.action_size = action_size
-
-        self.memory = []
-        self.gamma = 0.95  # discount rate
-        # self.epsilon = 1.0  # exploration rate
-
-        self.epsilon = 0.1
-
-        self.epsilon_min = 0.01
-        self.epsilon_decay = 0.995
-        self.learning_rate = 0.001
-        self.batch_size = 32
-
-        self.model = self._build_model()
-
-    def _build_model(self):
-        model = tf.keras.models.Sequential()
-        model.add(tf.keras.layers.Dense(24, input_dim=self.state_size, activation='relu'))
-        model.add(tf.keras.layers.Dense(24, activation='relu'))
-        model.add(tf.keras.layers.Dense(self.action_size, activation='linear'))
-        model.compile(loss='mse', optimizer=tf.keras.optimizers.Adam(lr=self.learning_rate))
-        return model
-
-    def remember(self, state, action, reward, next_state, done):
-        self.memory.append((state, action, reward, next_state, done))
-
-    def act(self, state):
-        state = np.reshape(state, [1, self.state_size])
-
-        if np.random.rand() <= self.epsilon:
-            return random.randrange(self.action_size)
-
-        act_values = self.model.predict(state, verbose=0)
-        return np.argmax(act_values[0])  # returns action
-
-    def train(self, experiences):
-        # Extract experiences
-        states, actions, rewards, next_states, dones = zip(*experiences)
-
-        # Convert data to numpy arrays
-        states = np.array(states).reshape(-1, self.state_size)
-        actions = np.array(actions)
-        rewards = np.array(rewards)
-        next_states = np.array(next_states).reshape(-1, self.state_size)
-        dones = np.array(dones)
-
-        # Compute the Q-values for the current states and next states
-        q_values = self.model.predict(states, verbose=0)
-        next_q_values = self.model.predict(next_states, verbose=0)
-
-        # Compute the target Q-values
-        targets = rewards + self.gamma * np.max(next_q_values, axis=1) * (1 - dones)
-
-        # Update the Q-values for the taken actions
-        rows = np.arange(len(actions))
-        q_values[rows, actions] = targets
-
-        # Train the model
-        self.model.train_on_batch(states, q_values)
-
 class ReplayBuffer:
     def __init__(self, capacity):
         self.capacity = capacity
@@ -206,57 +142,6 @@ class ReplayBuffer:
 
     def __len__(self):
         return len(self.buffer)
-
-
-def train_dqn_agent(env, num_episodes=10, batch_size=32, gamma=0.95):
-    state_size = env.observation_space.shape[0]
-    action_size = env.action_space.n
-    agent = DQNAgent(state_size, action_size)
-    memory_buffer = ReplayBuffer(capacity=100)
-    rewards = []
-
-    # Training
-    for episode in range(num_episodes):
-        curr_state = env.reset()
-        curr_state = np.reshape(curr_state, [1, state_size])  # Reshape for the DNN
-        total_reward = 0  # Reset total_reward for the episode
-        complete = False
-
-        print(f'Episode {episode}')
-
-        while not complete:
-            curr_action = agent.act(curr_state)  # Decide action
-            nxt_state, curr_reward, complete, _ = env.step(curr_action)  # Execute action
-            nxt_state = np.reshape(nxt_state, [1, state_size])  # Reshape for the DNN
-
-            # Store experience
-            memory_buffer.add(curr_state, curr_action, curr_reward, nxt_state, complete)
-
-            # Train the agent with a batch from the replay buffer
-            if len(memory_buffer) > batch_size:
-                experiences = memory_buffer.sample(batch_size)
-                agent.train(experiences)
-
-            curr_state = nxt_state
-            total_reward += curr_reward
-
-        rewards.append(total_reward)
-        print(f"Episode {episode + 1}: Total Reward = {total_reward}")
-
-    # Evaluation
-    total_test_reward = 0
-    curr_state = env.reset()
-    curr_state = np.reshape(curr_state, [1, state_size])  # Reshape for the DNN
-    complete = False
-
-    while not complete:
-        curr_action = agent.act(curr_state)  # Use act method for evaluation
-        nxt_state, curr_reward, complete, _ = env.step(curr_action)
-        nxt_state = np.reshape(nxt_state, [1, state_size])  # Reshape for the DNN
-        total_test_reward += curr_reward
-        curr_state = nxt_state
-    return rewards
-
 
 def visualize_training_results(rewards):
     """
@@ -284,13 +169,4 @@ def visualize_training_results(rewards):
     plt.show()
 
 if __name__ == '__main__':
-    env = IDSEnvironment()
-    training_rewards = train_dqn_agent(env)
-    visualize_training_results(training_rewards)
-    # for _ in range(10000):  # Try for 5 episodes
-    #     state = env.reset()
-    #     env.render()
-    #     action = env.action_space.sample()  # Sample random action
-    #     # print(f"Taking action: {'Do Nothing' if action == 0 else 'Raise Alarm'}")
-    #     next_state, reward, done, _ = env.step(action)
-    # print(f"Reward received: {reward}\n")
+    pass
