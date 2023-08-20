7a8cfcd6072fb5d5fbaf24046d180d6c23286bcb
sophiaabolore <59152413+sophiaabolore@users.noreply.github.com>
2023-08-20 13:06:20 -0700

    Updated job_script file

--- job_script.sh
+++ job_script.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-#SBATCH --job-name=any_name_here
+#SBATCH --job-name=DRL_for_IDS_experiments
 
 #SBATCH --partition=t4v2,rtx6000,a40
 
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:4
 
 #SBATCH --qos=normal
 
 #SBATCH --cpus-per-task=4
 
-#SBATCH --mem-per-cpu=8G
+#SBATCH --mem-per-cpu=32GB
 
 #SBATCH --output=slurm-%j.out
 
